ddfa84772e3520863e5d6a32d96560c91a1bc95b
Matthias <info@freepascal-meets-sdl.net>
2025-01-26 00:25:05 +0100

    Finish SDL_pen.inc

--- units/SDL3.pas
+++ units/SDL3.pas
@@ -95,7 +95,7 @@ const
 {$I SDL_mouse.inc}                        // 3.1.6-prev (unfinished)
 {$I SDL_keyboard.inc}                     // 3.1.6-prev (unfinished)
 {$I SDL_joystick.inc}                     // 3.1.6-prev (unfinished)
-{$I SDL_pen.inc}                          // 3.1.6-prev (unfinished)
+{$I SDL_pen.inc}                          // 3.1.6-prev
 {$I SDL_keycode.inc}                      // 3.1.6-prev (unfinished)
 {$I SDL_scancode.inc}                     // 3.1.6-prev
 {$I SDL_touch.inc}                        // 3.1.6-prev (unfinished)
