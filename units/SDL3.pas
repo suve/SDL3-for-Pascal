63b15294315fff1ec4d6ade52987895bdc05d446
Matthias <info@freepascal-meets-sdl.net>
2025-02-07 09:55:22 +0100

    Add SDL_platform.inc

--- units/SDL3.pas
+++ units/SDL3.pas
@@ -84,6 +84,7 @@ const
 {$I SDL_hints.inc}                        // 3.2.0
 {$I SDL_misc.inc}                         // 3.2.0
 {$I SDL_stdinc.inc}                       // 3.1.6-prev (unfinished)
+{$I SDL_platform.inc}                     // 3.2.0
 {$I SDL_loadso.inc}                       // 3.2.0
 {$I SDL_rect.inc}                         // 3.1.6-prev
 {$I SDL_properties.inc}                   // 3.1.6-prev
