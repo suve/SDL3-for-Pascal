a7d5b2e230876821ee3758fa8705286935fe3999
suve <veg@svgames.pl>
2025-06-15 12:02:10 +0200

    Add SDL3_gfx (imageFilter part)

---
+++ units/SDL3_gfx.pas
@@ -0,0 +1,51 @@
+unit SDL3_gfx;
+
+{
+  This file is part of:
+
+    SDL3 for Pascal
+    (https://github.com/PascalGameDevelopment/SDL3-for-Pascal)
+    SPDX-License-Identifier: Zlib
+}
+
+{$I sdl.inc}
+
+Interface
+  uses
+    {$IFDEF FPC}
+      ctypes,
+    {$ENDIF}
+    SDL3;
+
+const
+  {$IFDEF WINDOWS}
+    GFX_LibName = 'SDL3_gfx.dll';
+  {$ENDIF}
+
+  {$IFDEF UNIX}
+    {$IFDEF DARWIN}
+      GFX_LibName = 'libSDL3_gfx.dylib';
+      {$IFDEF FPC}
+        {$LINKLIB libSDL3_gfx}
+      {$ENDIF}
+    {$ELSE}
+      {$IFDEF FPC}
+        GFX_LibName = 'libSDL3_gfx.so';
+      {$ELSE}
+        GFX_LibName = 'libSDL3_gfx.so.0';
+      {$ENDIF}
+    {$ENDIF}
+  {$ENDIF}
+
+  {$IFDEF MACOS}
+    GFX_LibName = 'SDL3_gfx';
+    {$IFDEF FPC}
+      {$linklib libSDL3_gfx}
+    {$ENDIF}
+  {$ENDIF}
+
+{$INCLUDE SDL3_imageFilter.inc}
+
+Implementation
+
+End.
