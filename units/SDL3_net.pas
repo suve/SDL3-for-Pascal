4ef0cb1b5890db888736b78510b5f2aa2c6385c2
suve <veg@svgames.pl>
2026-07-01 22:59:00 +0200

    Add SDL3_net multi-socket polling API

--- units/SDL3_net.pas
+++ units/SDL3_net.pas
@@ -1590,6 +1590,61 @@ procedure NET_SimulateDatagramPacketLoss(sock: PNET_DatagramSocket; percent_loss
 procedure NET_DestroyDatagramSocket(sock: PNET_DatagramSocket); cdecl;
   external NET_LibName {$IFDEF DELPHI} {$IFDEF MACOS} name '_NET_DestroyDatagramSocket' {$ENDIF} {$ENDIF};
 
+{ -- multi-socket polling -- }
+
+(*
+ * Block on multiple sockets until at least one has data available.
+ *
+ * This is a complex function that most apps won't need, but it could be used
+ * to implement a more efficient server or i/o thread in some cases.
+ *
+ * This allows you to give it a list of objects and wait for new input to
+ * become available on any of them. The calling thread is put to sleep until
+ * such a time.
+ *
+ * The following things can be specified in the `vsockets` array, cast to
+ * `void *`:
+ *
+ * - NET_Server (reports new input when a connection is ready to be accepted
+ *   with NET_AcceptClient())
+ * - NET_StreamSocket (reports new input when the remote end has sent more
+ *   bytes of data to be read with NET_ReadFromStreamSocket, or if the socket
+ *   finished making its initial connection).
+ * - NET_DatagramSocket (reports new input when a new packet arrives that can
+ *   be read with NET_ReceiveDatagram).
+ *
+ * This function takes a timeout value, represented in milliseconds, of how
+ * long to wait for resolution to complete. Specifying a timeout of -1
+ * instructs the library to wait indefinitely, and a timeout of 0 just checks
+ * the current status and returns immediately.
+ *
+ * This returns the number of items that have new input, but it does not tell
+ * you which ones; since access to them is non-blocking, you can just try to
+ * read from each of them and see which are ready. If nothing is ready and the
+ * timeout is reached, this returns zero. On error, this returns -1.
+ *
+ * \param vsockets an array of pointers to various objects that can be waited
+ *                 on, each cast to a void pointer.
+ * \param numsockets the number of pointers in the `vsockets` array.
+ * \param timeout Number of milliseconds to wait for new input to become
+ *                available. -1 to wait indefinitely, 0 to check once without
+ *                waiting.
+ * \returns the number of items that have new input, or -1 on error.
+ *
+ * \threadsafety You should not operate on the same socket from multiple
+ *               threads at the same time without supplying a serialization
+ *               mechanism. However, different threads may access different
+ *               sockets at the same time without problems.
+ *
+ * \since This function is available since SDL_net 3.0.0.
+ *
+ * \sa NET_CreateDatagramSocket
+ * \sa NET_SendDatagram
+ * \sa NET_ReceiveDatagram
+ *)
+function NET_WaitUntilInputAvailable(vsockets: PPointer; numsockets: cint; timeout: cint32): cint; cdecl;
+  external NET_LibName {$IFDEF DELPHI} {$IFDEF MACOS} name '_NET_WaitUntilInputAvailable' {$ENDIF} {$ENDIF};
+
 
 implementation
 
